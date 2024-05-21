9c37f2808c083ce427a85a0f7a4eace65212070d
Aleksandar Gospavić <151999326+gospaleks@users.noreply.github.com>
2024-05-21 22:44:28 +0200

    Primeri sa lab-a

--- Lab/Lab-4/Primeri Lab/GraphInt.cpp
+++ Lab/Lab-4/Primeri Lab/GraphInt.cpp
@@ -6,6 +6,200 @@ using namespace std;
 #include "QueueAsArrayInt.h"
 #include "StackAsArrayInt.h"
 
+// Primeri sa lab vezbi
+
+bool GraphAsListsInt::findCycleWithNode(const int& data)
+{
+	/*
+		Odredjuje da li postoji (i koji je) ciklus koji sadrzi cvor data.
+	*/
+
+	// Postavi status svim cvorovima na 0
+	setStatusForAllNodes(0);
+
+	// Postavi prev na null svim cvorovima
+	LinkedNodeInt* ptr = start;
+	while (ptr != nullptr) {
+		ptr->prev = nullptr;
+		ptr = ptr->next;
+	}
+
+	// Pronadji cvor data od koga krece trazenje ciklusa
+	ptr = findNode(data);
+
+	if (ptr == nullptr)
+		return false;
+
+	StackAsArrayLinkedNodeInt stek(nodeNum);
+	ptr->status = 1;
+	stek.push(ptr);
+	while (!stek.isEmpty()) {
+		ptr = stek.pop();
+		LinkedEdgeInt* pEdge = ptr->adj;
+		while (pEdge != nullptr) {
+
+			if (pEdge->dest->status == 1) {
+				// Ako je pronadjen ciklus
+				// Ispisi putanju
+				cout << "Ciklus: ";
+				LinkedNodeInt* ptrTmp = ptr;
+				while (ptrTmp != nullptr) {
+					cout << ptrTmp->node << " ";
+					ptrTmp = ptrTmp->prev;
+				}
+				return true;
+			}
+
+			if (pEdge->dest->status == 0) {
+				pEdge->dest->status = 1;
+				pEdge->dest->prev = ptr;
+				stek.push(pEdge->dest);
+			}
+
+			pEdge = pEdge->link;
+		}
+	}
+
+	cout << "Nema ciklusa!" << endl;
+	return false;
+}
+
+void GraphAsListsInt::findCycleThroughNode(int dataStart, int dataEnd, int dataOnPath)
+{
+	/*
+		Realizovati metod koji odredjuje i prikazuje jedan (bilo koji) put
+		u grafu izmedju zadatog pocetnog i krajnjeg cvora koji prolazi 
+		kroz treci zadati cvor i ne mora biti prost
+	*/
+
+	setStatusForAllNodes(0);
+
+	// Postavi prev na null svim cvorovima
+	LinkedNodeInt* ptr = start;
+	while (ptr != nullptr) {
+		ptr->prev = nullptr;
+		ptr = ptr->next;
+	}
+
+	ptr = findNode(dataStart);
+
+	// Nije pronadjen pocetni ili krajnji cvor
+	if (ptr == nullptr)
+		return;
+
+	bool foundDataOnPath = false;
+
+	StackAsArrayLinkedNodeInt stek(nodeNum);
+	ptr->status = 1;
+	stek.push(ptr);
+	while (!stek.isEmpty()) {
+
+		ptr = stek.pop();
+		LinkedEdgeInt* pEdge = ptr->adj;
+		while (pEdge != nullptr) {
+
+			if (pEdge->dest->status == 0) {
+				pEdge->dest->status = 1;
+				pEdge->dest->prev = ptr;
+				stek.push(pEdge->dest);
+			}
+
+			// ako je naso granu ga cvoru dataOnPath
+			// ne dozvoli dfs-u da prolazi ostale grane jer put mora da prodje kroz tu granu
+			// zato ide break
+			if (pEdge->dest->node == dataOnPath) {
+				foundDataOnPath = true;
+				break;
+			}
+
+			// ako je doso do krajnjeg cvora
+			if (pEdge->dest->node == dataEnd) {
+				// ako je proso onaj treci cvor usput ispisi put
+				if (foundDataOnPath) {
+					cout << "Put: ";
+					cout << dataEnd << " ";
+					LinkedNodeInt* ptrTmp = ptr;
+					while (ptrTmp != nullptr) {
+						cout << ptrTmp->node << " ";
+						ptrTmp = ptrTmp->prev;
+					}
+					cout << endl;
+				}
+				return;
+			}
+
+			pEdge = pEdge->link;
+		}
+
+	}
+
+}
+
+// TODO: Realizovati metod koji vraca da li postoji i odredjuje jedan (bilo koji)
+// put u grafu od zadatog pocetnog (dataFirst) do zadatog krajnjeg cvora (dataLast)
+// koji ne prolazi kroz zadati poteg.
+bool GraphAsListsInt::pathWithoutEdge(int dataFirst, int dataLast, int dataEgdeSource, int dataEdgeDest)
+{
+	LinkedNodeInt* ptr = start;
+
+	// postavi status na 0 i prev na NULL
+	while (ptr != nullptr) {
+		ptr->status = 0;
+		ptr->prev = nullptr;
+		ptr = ptr->next;
+	}
+
+	ptr = findNode(dataFirst);
+	
+	if (ptr == nullptr)
+		return false;
+
+	StackAsArrayLinkedNodeInt stek(nodeNum);
+	ptr->status = 1;
+	stek.push(ptr);
+
+	while (!stek.isEmpty()) {
+		ptr = stek.pop();
+		LinkedEdgeInt* pEdge = ptr->adj;
+
+		while (pEdge != nullptr) {
+
+			// ako je to taj poteg ne dodaj ga na stek (KLJUCNI DEO)
+			if (ptr->node == dataEgdeSource && pEdge->dest->node == dataEdgeDest) {
+				pEdge = pEdge->link;
+				continue;
+			}
+
+			// Oznaci cvor kao posecen
+			if (pEdge->dest->status == 0) {
+				pEdge->dest->status = 1;
+				pEdge->dest->prev = ptr;
+				stek.push(pEdge->dest);
+			}
+
+			// Nadjen put
+			if (pEdge->dest->node == dataLast) {
+				cout << "Put: ";
+				cout << dataLast << " ";
+				LinkedNodeInt* ptrTmp = ptr;
+				while (ptrTmp != nullptr) {
+					cout << ptrTmp->node << " ";
+					ptrTmp = ptrTmp->prev;
+				}
+				cout << endl;
+				return true;
+			}
+
+
+			pEdge = pEdge->link;
+		}
+	}
+
+	cout << "Nije nadjen takav put." << endl;
+	return false;
+}
+
+
 GraphAsListsInt::GraphAsListsInt() : start(nullptr), nodeNum()
 {
 }
@@ -315,197 +509,4 @@ long GraphAsListsInt::topologicalOrderTravrsal() const
 	}
 	cout << endl;
 	return retVal;
-}
-
-// Primeri sa lab vezbi
-
-bool GraphAsListsInt::findCycleWithNode(const int& data)
-{
-	/*
-		Odredjuje da li postoji (i koji je) ciklus koji sadrzi cvor data.
-	*/
-
-	// Postavi status svim cvorovima na 0
-	setStatusForAllNodes(0);
-
-	// Postavi prev na null svim cvorovima
-	LinkedNodeInt* ptr = start;
-	while (ptr != nullptr) {
-		ptr->prev = nullptr;
-		ptr = ptr->next;
-	}
-
-	// Pronadji cvor data od koga krece trazenje ciklusa
-	ptr = findNode(data);
-
-	if (ptr == nullptr)
-		return false;
-
-	StackAsArrayLinkedNodeInt stek(nodeNum);
-	ptr->status = 1;
-	stek.push(ptr);
-	while (!stek.isEmpty()) {
-		ptr = stek.pop();
-		LinkedEdgeInt* pEdge = ptr->adj;
-		while (pEdge != nullptr) {
-
-			if (pEdge->dest->status == 1) {
-				// Ako je pronadjen ciklus
-				// Ispisi putanju
-				cout << "Ciklus: ";
-				LinkedNodeInt* ptrTmp = ptr;
-				while (ptrTmp != nullptr) {
-					cout << ptrTmp->node << " ";
-					ptrTmp = ptrTmp->prev;
-				}
-				return true;
-			}
-
-			if (pEdge->dest->status == 0) {
-				pEdge->dest->status = 1;
-				pEdge->dest->prev = ptr;
-				stek.push(pEdge->dest);
-			}
-
-			pEdge = pEdge->link;
-		}
-	}
-
-	cout << "Nema ciklusa!" << endl;
-	return false;
-}
-
-void GraphAsListsInt::findCycleThroughNode(int dataStart, int dataEnd, int dataOnPath)
-{
-	/*
-		Realizovati metod koji odredjuje i prikazuje jedan (bilo koji) put
-		u grafu izmedju zadatog pocetnog i krajnjeg cvora koji prolazi 
-		kroz treci zadati cvor i ne mora biti prost
-	*/
-
-	setStatusForAllNodes(0);
-
-	// Postavi prev na null svim cvorovima
-	LinkedNodeInt* ptr = start;
-	while (ptr != nullptr) {
-		ptr->prev = nullptr;
-		ptr = ptr->next;
-	}
-
-	ptr = findNode(dataStart);
-
-	// Nije pronadjen pocetni ili krajnji cvor
-	if (ptr == nullptr)
-		return;
-
-	bool foundDataOnPath = false;
-
-	StackAsArrayLinkedNodeInt stek(nodeNum);
-	ptr->status = 1;
-	stek.push(ptr);
-	while (!stek.isEmpty()) {
-
-		ptr = stek.pop();
-		LinkedEdgeInt* pEdge = ptr->adj;
-		while (pEdge != nullptr) {
-
-			if (pEdge->dest->status == 0) {
-				pEdge->dest->status = 1;
-				pEdge->dest->prev = ptr;
-				stek.push(pEdge->dest);
-			}
-
-			// ako je naso granu ga cvoru dataOnPath
-			// ne dozvoli dfs-u da prolazi ostale grane jer put mora da prodje kroz tu granu
-			// zato ide break
-			if (pEdge->dest->node == dataOnPath) {
-				foundDataOnPath = true;
-				break;
-			}
-
-			// ako je doso do krajnjeg cvora
-			if (pEdge->dest->node == dataEnd) {
-				// ako je proso onaj treci cvor usput ispisi put
-				if (foundDataOnPath) {
-					cout << "Put: ";
-					cout << dataEnd << " ";
-					LinkedNodeInt* ptrTmp = ptr;
-					while (ptrTmp != nullptr) {
-						cout << ptrTmp->node << " ";
-						ptrTmp = ptrTmp->prev;
-					}
-					cout << endl;
-				}
-				return;
-			}
-
-			pEdge = pEdge->link;
-		}
-
-	}
-
-}
-
-// TODO: Realizovati metod koji vraca da li postoji i odredjuje jedan (bilo koji)
-// put u grafu od zadatog pocetnog (dataFirst) do zadatog krajnjeg cvora (dataLast)
-// koji ne prolazi kroz zadati poteg.
-bool GraphAsListsInt::pathWithoutEdge(int dataFirst, int dataLast, int dataEgdeSource, int dataEdgeDest)
-{
-	LinkedNodeInt* ptr = start;
-
-	// postavi status na 0 i prev na NULL
-	while (ptr != nullptr) {
-		ptr->status = 0;
-		ptr->prev = nullptr;
-		ptr = ptr->next;
-	}
-
-	ptr = findNode(dataFirst);
-	
-	if (ptr == nullptr)
-		return false;
-
-	StackAsArrayLinkedNodeInt stek(nodeNum);
-	ptr->status = 1;
-	stek.push(ptr);
-
-	while (!stek.isEmpty()) {
-		ptr = stek.pop();
-		LinkedEdgeInt* pEdge = ptr->adj;
-
-		while (pEdge != nullptr) {
-
-			// ako je to taj poteg ne dodaj ga na stek (KLJUCNI DEO)
-			if (ptr->node == dataEgdeSource && pEdge->dest->node == dataEdgeDest) {
-				pEdge = pEdge->link;
-				continue;
-			}
-
-			// Oznaci cvor kao posecen
-			if (pEdge->dest->status == 0) {
-				pEdge->dest->status = 1;
-				pEdge->dest->prev = ptr;
-				stek.push(pEdge->dest);
-			}
-
-			// Nadjen put
-			if (pEdge->dest->node == dataLast) {
-				cout << "Put: ";
-				cout << dataLast << " ";
-				LinkedNodeInt* ptrTmp = ptr;
-				while (ptrTmp != nullptr) {
-					cout << ptrTmp->node << " ";
-					ptrTmp = ptrTmp->prev;
-				}
-				cout << endl;
-				return true;
-			}
-
-
-			pEdge = pEdge->link;
-		}
-	}
-
-	cout << "Nije nadjen takav put." << endl;
-	return false;
 }
